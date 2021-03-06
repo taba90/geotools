2cc037d4da1896ad24254e96985274fb51cbc3c8
Marco Volpini <tabareu90@gmail.com>
2021-03-06 20:20:38 +0100

    cache deep copy

--- modules/plugin/mongodb/src/main/java/org/geotools/data/mongodb/complex/schemaless/SchemalessFeatureTypeCache.java
+++ modules/plugin/mongodb/src/main/java/org/geotools/data/mongodb/complex/schemaless/SchemalessFeatureTypeCache.java
@@ -3,6 +3,7 @@ package org.geotools.data.mongodb.complex.schemaless;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
+import com.google.gson.Gson;
 import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
@@ -11,7 +12,8 @@ import org.opengis.feature.type.Name;
 public class SchemalessFeatureTypeCache {
 
     private static SchemalessFeatureTypeCache schemalessCache;
-    private final LoadingCache<Name, Optional<ModifiableComplexFeatureType>> cache;
+    private final LoadingCache<Name, Optional<String>> cache;
+    private Gson gson;
 
     private SchemalessFeatureTypeCache() {
         cache =
@@ -20,23 +22,26 @@ public class SchemalessFeatureTypeCache {
                         .initialCapacity(1)
                         .expireAfterAccess(120, TimeUnit.MINUTES)
                         .build(
-                                new CacheLoader<Name, Optional<ModifiableComplexFeatureType>>() {
+                                new CacheLoader<Name, Optional<String>>() {
                                     @Override
-                                    public Optional<ModifiableComplexFeatureType> load(Name key) {
+                                    public Optional<String> load(Name key) {
                                         return Optional.empty();
                                     }
                                 });
+        this.gson = new Gson();
     }
 
     public void updateCache(Name key, ModifiableComplexFeatureType featureType)
             throws ExecutionException {
-        this.cache.asMap().put(key, Optional.of(featureType));
+        String json = gson.toJson(featureType);
+        this.cache.asMap().put(key, Optional.of(json));
     }
 
-    public ModifiableComplexFeatureType getFeatureType(Name key)
-            throws ExecutionException, CloneNotSupportedException {
-        ModifiableComplexFeatureType featureType = this.cache.get(key).orElseGet(() -> null);
-        if (featureType != null) return (ModifiableComplexFeatureType) featureType.clone();
+    public ModifiableComplexFeatureType getFeatureType(Name key) throws ExecutionException {
+        String featureType = this.cache.get(key).orElseGet(() -> null);
+        ModifiableComplexFeatureType mcft =
+                this.gson.fromJson(featureType, ModifiableComplexFeatureType.class);
+        if (mcft != null) return mcft;
         return null;
     }
 
